00cb62f0800e38311c09a469569946c6db0020a7
João Gabriel <belelgamer12br@gmail.com>
2026-07-22 12:24:19 -0300

    refactor(main): validacao da entrada agora retorna a string da operacao && validação do arquivo de saída

--- src/main.c
+++ src/main.c
@@ -22,12 +22,12 @@ const static char* help_message = (
     NEGRITO "ruffman help" RESET
 );
 
-
-int validar_argumentos_entrada(const int argc, const char* argv[]){
+//retorna a string da operação a ser realizada
+char* validar_operacao_entrada(const int argc, const char* argv[]){
     if(argc < 2 || argc > 4){
         puts(NEGRITO RED "ERRO" RESET);
         printf(RED "Número de argumentos de entrada inválido: %d \n\n"RESET,argc);
-        return 0;
+        return NULL;
     }
     //permite o usuário digitar o comando de forma case insensitive
     char* arg1 = to_lower(argv[1]);
@@ -38,29 +38,67 @@ int validar_argumentos_entrada(const int argc, const char* argv[]){
         if(argc > 2){
             puts(RED "ERRO: Número de argumentos excessivo, esperado somente 'help'" RESET);
             free(arg1);
-            return 0;
+            return NULL;
         }
-        free(arg1);
-        return 1;
+        return arg1;
     }
     else if(!strcmp(arg1,ZIP) || !strcmp(arg1,UNZIP)){
         if(argc == 2){
             puts(RED "ERRO: Esperado caminho do arquivo de entrada como terceiro argumento" RESET);
             free(arg1);
-            return 0;
+            return NULL;
         }
-        free(arg1);
-        return 1;
+        return arg1;
     }
     else{
         puts(RED "ERRO: COMANDO DESCONHECIDO: " RESET);
         printf("%s\n",arg1);
         free(arg1);
-        return 0;
+        return NULL;
     }
 }
 
-int main(int argc, int argv){
-    if(!validar_argumentos_entrada(argc,argv)){
+int main(int argc, char* argv){
+    char* operacao = validar_operacao_entrada(argc,argv);
+    if (operacao == NULL){
+        printf("\nUse 'ruffman help' para verificar o uso do programa\n");
+        return 1;
     }
+    //caso seja apenas comando de help
+    if (!strcmp(operacao,"help")){
+        free(operacao);
+        puts(help_message);
+        return 0;
+    }
+    FILE* arquivo_entrada = fopen(argv[2],"r");
+    if(arquivo_entrada == NULL){
+        free(operacao);
+        puts(
+            RED "ERRO: ARQUIVO NÃO ENCONTRADO, "
+            "VERIFIQUE SE O ARQUIVO EXISTE OU SE O CAMINHO ESTÁ CORRETO"RESET);
+        return 1;
+    }
+    //caso o caminho de saída não seja especificado
+    //o caminho padrão será o diretório atual
+    char* output_path = (argc == 3) ? "./" : argv[3];
+    int return_value = 0;
+ 
+    if(!strcmp(operacao,ZIP)){
+        return_value = encode(arquivo_entrada,output_path);
+    }
+    else{
+        return_value = decode(arquivo_entrada,output_path);
+    }
+    fclose(arquivo_entrada);
+    FILE* validacao_saida = fopen(output_path,"r");
+    if(validacao_saida == NULL){
+        puts(
+            RED "ERRO: ARQUIVO DE SAÍDA NÃO FOI CRIADO, "
+            "VERIFIQUE SE O DIRETÓRIO EXISTE E QUE VOCÊ TENHA PERMISSÃO DE ESCRITA" RESET
+        );
+    }
+    fclose(validacao_saida);
+
+    return return_value;
+
 }
