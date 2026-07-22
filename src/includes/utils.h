7f6346ea966d5d3754b2925d9163d88a11c8c52a
João Gabriel <belelgamer12br@gmail.com>
2026-07-22 14:59:57 -0300

    feat(utils): adiciona função e struct da tela de carregamento

--- src/includes/utils.h
+++ src/includes/utils.h
@@ -2,5 +2,10 @@
 
 char* to_lower(const char* string);
 
+typedef struct carregamento_options LoadingOpt;
+LoadingOpt* new_LoadingOptions(char* message, unsigned char* flag_concluido);
+//recebe um parâmetro do tipo LoadingOpt* obrigatoriamente
+void* tela_carregamento(void* load_options);
+
 #define UTILS_H
 #endif
--- src/utils.c
+++ src/utils.c
@@ -1,6 +1,7 @@
 #include "utils.h"
 #include <stdlib.h>
 #include <ctype.h>
+#include <unistd.h>
 
 char* to_lower(const char* string){
     if(string == NULL){
@@ -11,4 +12,43 @@ char* to_lower(const char* string){
         new_string[i] = tolower(string[i]);
     }
     return new_string; 
+}
+
+struct carregamento_options {
+    char* message;
+    unsigned char flag_concluido;
+};
+
+//flag é obrigatória
+LoadingOpt* new_LoadingOptions(char* message, unsigned char* flag_concluido){
+    if(flag_concluido == NULL){
+        return NULL;
+    }
+    LoadingOpt* newLO = (LoadingOpt*) malloc(sizeof(LoadingOpt));
+    newLO->flag_concluido = flag_concluido;
+    newLO->message= message;
+    return newLO;
+}
+void* tela_carregamento(void* load_options){
+    if(load_options == NULL){
+        puts("Erro ao iniciar tela de carregamento: Null load_options");
+        return;
+    }
+    LoadingOpt* options = (LoadingOpt*) load_options;
+    char* loading_carrossel[] = {
+        "|          |",
+        "|>         |",
+        "|->        |",
+        "|  -->     |",
+        "|     -->  |",
+        "|        --|",
+    };
+
+    int loading_carrosel_size = sizeof(loading_carrossel) / sizeof(char*);
+    int stage = 0;
+    while(!options->flag_concluido){
+        printf("%s %s",loading_carrossel[stage++ % loading_carrosel_size],options->message);
+        sleep(1);
+    }
+    return ;
 }
