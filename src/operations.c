423c48890f8615ec264290dab723b672a3a6f485
João Gabriel <belelgamer12br@gmail.com>
2026-07-28 15:32:40 -0300

    feat/fix(operations): adiciona implementação de descompactação e consertos minimos

--- src/operations.c
+++ src/operations.c
@@ -7,39 +7,12 @@
 #include "operations.h"
 #include "utils.h"
 
-/*Essas funções encapsulam as funções de compactação/descompactação
-com a tela de carregamento*/
-
-int encode(char* inputpath, char* outputpath){
-    atomic_uchar flag_concluido;
-    atomic_store(&flag_concluido,0);
-    pthread_t thread_carregamento;
-    LoadingOpt* lopt = new_LoadingOptions("COMPACTANDO ARQUIVO",&flag_concluido);
-    pthread_create(&thread_carregamento,NULL,tela_carregamento,(void*) lopt);
-    int sucess = __encode(inputpath,outputpath);
-    atomic_store(&flag_concluido,1);
-    pthread_join(thread_carregamento,NULL);
-    return sucess;
-}
-
-int decode(char* inputpath, char* outputpath){
-    atomic_uchar flag_concluido;
-    atomic_store(&flag_concluido,0);
-    pthread_t thread_carregamento;
-    LoadingOpt* lopt = new_LoadingOptions("DESCOMPACTANDO ARQUIVO ARQUIVO",&flag_concluido);
-    pthread_create(&thread_carregamento,NULL,tela_carregamento,(void*) lopt);
-    int sucess = __decode(inputpath,outputpath);
-    atomic_store(&flag_concluido,1);
-    pthread_join(thread_carregamento,NULL);
-    return sucess;
-}
-
 //TODO: alterar prints de erro nas funcoes para logs para n atrapalhar 
 //a tela de carregamento
 
 
 void generate_meta_data(FILE* file, char* original_file_name){
-    if(!file) return NULL;
+    if(!file) return;
     const char* meta_string = (
         "#Aviso: Não altere esse arquivo manualmente\n"
         "original_file_name:%s\n"
@@ -89,7 +62,7 @@ int __encode(char* inputpath, char* outputpath){
     fclose(output_file);
     destroy_RuffVector(heap);
     arvore = NULL;
-    for(int i = 0; i < (sizeof(bytes_compactados) / sizeof(bytes_compactados[0])); i++){
+    for(int i = 0; i < 256; i++){
         free_BytesCompactados(bytes_compactados[i]);
     }
     free(bytes_compactados);
@@ -98,5 +71,101 @@ int __encode(char* inputpath, char* outputpath){
 }
 
 int __decode(char* inputpath, char* outputpath){
+    FILE* input_file = fopen(inputpath,"rb"); if(!input_file) return 0;
+    FILE* output_file = NULL;
+    char* original_file_name = NULL;
+    char* input_serializer_version = NULL;
+    Ruff_Node* arvore = NULL;
+    char* input_file_data = NULL;
+    char buffer[1024];
 
+    while(fgets(buffer,sizeof(buffer),input_file) != NULL){
+        if(buffer[0]=="#") continue;
+
+        char* chave = strtok(buffer,":");
+        char* valor = strtok(NULL,"\n");
+
+        if(!strcmp(chave,"original_file_name")){
+            original_file_name = valor;
+            continue;
+        }
+        if(!strcmp(chave,"current_serializer_version")){
+            input_serializer_version = valor;
+            continue;
+        }
+        if(!strcmp(chave,"tree")){
+            arvore = desserializar_arvore(input_file);
+            fseek(input_file,1,SEEK_CUR); 
+            // avança o cursor em 1 posição por causa do \n
+            continue;
+        }
+        if(!strcmp(chave,"data")){
+            long tamanho_dados = bytes_til_EOF(input_file);
+            if(!tamanho_dados) return 0;
+            input_file_data = (char*) malloc(tamanho_dados);
+            fread(input_file_data,1,tamanho_dados,input_file);
+            continue;
+        }
+        puts("ERRO DURANTE PARSING DO ARQUIVO DE ENTRADA, SEÇÃO DESCONHECIDA");
+        return 0;
+    }
+
+    if(
+           !original_file_name 
+        || !input_serializer_version
+        || !arvore
+        || !input_file_data
+    ){
+        puts(
+            "ERRO DURANTE PARSING DO ARQUIVO DE ENTRADA, "
+            "ELEMENTOS ESSENCIAIS PARA DESCOMPACTAÇÃO FALTANDO"
+        );
+        return 0;
+    }
+
+    if(atoi(input_serializer_version) != CURRENT_SERIALIZER_VERSION){
+        puts("ERRO DURANTE DESCOMPACTAÇÃO: VERSÃO INCOMPATÍVEL");
+        return 0;
+    }
+
+    char* output_full_path = join_file_to_path(original_file_name,outputpath);
+
+    output_file = fopen(output_full_path,"wb");
+    int sucess = uncompact_data(arvore,input_file_data,output_file);
+
+    fclose(output_file);
+    fclose(input_file);
+    free(original_file_name);
+    free(input_serializer_version);
+    free(input_file_data);
+    free_RuffTree(arvore);
+
+    return sucess;
+}
+
+/*Essas funções encapsulam as funções de compactação/descompactação
+com a tela de carregamento*/
+
+int encode(char* inputpath, char* outputpath){
+    atomic_uchar flag_concluido;
+    atomic_store(&flag_concluido,0);
+    pthread_t thread_carregamento;
+    LoadingOpt* lopt = new_LoadingOptions("COMPACTANDO ARQUIVO",&flag_concluido);
+    pthread_create(&thread_carregamento,NULL,tela_carregamento,(void*) lopt);
+    int sucess = __encode(inputpath,outputpath);
+    atomic_store(&flag_concluido,1);
+    pthread_join(thread_carregamento,NULL);
+    return sucess;
+}
+
+int decode(char* inputpath, char* outputpath){
+    atomic_uchar flag_concluido;
+    atomic_store(&flag_concluido,0);
+    pthread_t thread_carregamento;
+    LoadingOpt* lopt = new_LoadingOptions("DESCOMPACTANDO ARQUIVO ARQUIVO",&flag_concluido);
+    pthread_create(&thread_carregamento,NULL,tela_carregamento,(void*) lopt);
+    int sucess = __decode(inputpath,outputpath);
+    atomic_store(&flag_concluido,1);
+    pthread_join(thread_carregamento,NULL);
+    return sucess;
 }
