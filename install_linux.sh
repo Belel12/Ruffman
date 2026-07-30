53661057238a42cdc1e2df2a4a3ce7bee708f003
João Gabriel <belelgamer12br@gmail.com>
2026-07-30 14:18:55 -0300

    refactor(install): adiciona encadeamento de operações

--- install_linux.sh
+++ install_linux.sh
@@ -1,16 +1,16 @@
-#compila o executavel
+# compila o executavel
 gcc -o bin/ruffman \
     src/main.c \
     src/ruffman.c \
     src/utils.c \
     src/operations.c \
-    -Isrc/includes/
+    -Isrc/includes/ && \
 
-#copia o executavel para o path do usuário para que ele possa
-#chamar  o programa de qualquer diretório
-cp bin/ruffman ~/.local/bin/
+# copia o executavel para o PATH do usuário
+# para chamar o programa de qualquer diretório
+cp bin/ruffman ~/.local/bin/ && \
 
-#cria o arquivo de desinstalação
+# cria o arquivo de desinstalação
 printf "rm -f ~/.local/bin/ruffman\n\
-        rm -f ./bin/* \n\
-        rm -f ./uninstall_linux.sh" > ./uninstall_linux.sh
+rm -f ./bin/*\n\
+rm -f ./uninstall_linux.sh\n" > ./uninstall_linux.sh
